6d3f88c14f101ea2d82af93bd484dabb18bda70f
saul zarate <saul.zarate@davinci.edu.ar>
2021-05-18 16:29:42 -0300

    atributos de producto_id y usuario_id agregado con sus getters y setters

--- models/Compra.py
+++ models/Compra.py
@@ -5,7 +5,10 @@ class Compra(Model):
 
     def __init__(self):
         self.__id = ''
+        self.__cantidad = ''
         self.__precioTotal = ''
+        self.__producto_id = ''
+        self.__usuario_id = ''
         self.__conection = Conection().get_conection()
     
     def getAllForUsuarioId(self, usuario_id):
@@ -28,10 +31,24 @@ class Compra(Model):
     """
     def get_id(self):
         return self.__id
+    def get_cantidad(self):
+        return self.__cantidad
     def get_precioTotal(self):
         return self.__precioTotal
-        
+    def get_producto_id(self):
+        return self.__producto_id
+    def get_usuario_id(self):
+        return self.__usuario_id
+    
+
     def set_id(self, id):
         self.__id = id
+    def set_cantidad(self, cantidad):
+        self.__cantidad = cantidad
     def set_precioTotal(self, precioTotal):
         self.__precioTotal = precioTotal
+    def set_producto_id(self, producto_id):
+        self.__producto_id = producto_id
+    def set_usuario_id(self, usuario_id):
+        self.__usuario_id = usuario_id
+    
