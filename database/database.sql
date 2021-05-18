c654be976d60a770e70fa3dd2bb06ed20284de4a
saul zarate <saul.zarate@davinci.edu.ar>
2021-05-18 16:43:02 -0300

    Insert de pais, provincias y ciudades

--- database/database.sql
+++ database/database.sql
@@ -18,8 +18,6 @@ CREATE TABLE productos(
 	FOREIGN KEY (categoria_id) REFERENCES categorias(id),
     FOREIGN KEY (marca_id) REFERENCES marcas(id)
 );
-
-
 CREATE TABLE paises(
     id int(100) AUTO_INCREMENT NOT NULL,
     nombre varchar(100) not null,
@@ -60,3 +58,13 @@ CREATE TABLE compras(
     FOREIGN KEY (producto_id) REFERENCES productos(id),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~ INSERTS ~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+-- Paises
+INSERT INTO paises (id, 'nombre') VALUES (54, 'Argentina'), (598, 'Uruguay'), (34, 'España'), (49, 'Alemania');
+-- Provincias
+INSERT INTO provincias (id, nombre, pais_id) VALUES(1,'CABA',54),(5,'Santa Fe',54),(8,'La Pampa',54),(96,'Cataluña',34),(75,'Colonia',598),(77,'Montevideo',598);
+-- Ciudades
+INSERT INTO ciudades (id, nombre, provincia_id) VALUES(1,'CABA',1),(2,'Rosario',5),(3,'Santa Rosa',8),(71,'Barcelona',96),(12,'Carmelo',75),(15,'Montevideo',77);
